d8bc5997b14c74c05c786dd92ca208b2f74fd6d8
khartoumsylla <100222432+khartoumsylla@users.noreply.github.com>
2023-06-08 12:33:51 +0200

    Update author.js

--- routes/author.js
+++ routes/author.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const Commentaire = require('../models/Commentaire')
+const Tag = require('../models/Tag')
 
-
-
+//requet accepte que des chiffre '/:id(\\d+)'
 
 // Route /author/commentaire
-router.route('/commentaire')
+router.route('/:author/commentaire')
      // Récupération des commentaires d'un auteur spécifique
      .get(async (req, res) => {
-        const author = req.query.auteur;
+        const author = req.params.author;
         const result = await Commentaire.allAuteur(author);
         res.send(result);
     });
