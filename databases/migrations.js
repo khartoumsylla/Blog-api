a7912efb351546aac135220ac535cb74af2f486f
khartoumsylla <100222432+khartoumsylla@users.noreply.github.com>
2023-06-08 12:41:43 +0200

    Update migrations.js

--- databases/migrations.js
+++ databases/migrations.js
@@ -1,7 +1,7 @@
 const db = require('./db')
 
 db.serialize(() => {
-  
+    db.run('DROP TABLE IF EXISTS articles');
     db.run("CREATE TABLE IF NOT EXISTS articles(   \
         id INTEGER PRIMARY KEY AUTOINCREMENT,   \
         titre VARCAHR(50) NOT NULL, \
@@ -10,6 +10,7 @@ db.serialize(() => {
          date_updated DATETIME , \
           visible BOOLEAN   \
     )")
+    db.run('DROP TABLE IF EXISTS commentaires');
     db.run("CREATE TABLE IF NOT EXISTS commentaires(   \
         id INTEGER PRIMARY KEY AUTOINCREMENT,   \
         id_article INTEGER, \
@@ -19,6 +20,8 @@ db.serialize(() => {
         updated_at DATETIME, \
         FOREIGN KEY (id_article) REFERENCES articles(id)   \
     )")
+
+    db.run('DROP TABLE IF EXISTS tags');
     db.run("CREATE TABLE IF NOT EXISTS tags(   \
         id INTEGER PRIMARY KEY AUTOINCREMENT,   \
         id_article INTEGER, \
